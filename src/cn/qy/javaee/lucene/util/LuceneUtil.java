ccd37c44a5e4c08256e7af7006ec52b9377e14b6
quyuan <quyuan891211@163.com>
2018-01-12 11:11:15 +0800

    LuceneUtil

--- src/cn/qy/javaee/lucene/util/LuceneUtil.java
+++ src/cn/qy/javaee/lucene/util/LuceneUtil.java
@@ -14,49 +14,44 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class LuceneUtil {
-    private static Directory directory;
-    private static Analyzer analyzer;
-    private static IndexWriter.MaxFieldLength maxFieldLength;
+    private  static Directory directory;
+    private  static Analyzer analyzer;
+    private  static IndexWriter.MaxFieldLength maxFieldLength;
+    private static Version version;
     static {
         try{
-
-            directory = FSDirectory.open(new File("D:/WebProjects/qy/IdeaProjects/MyLuence/LuceneDBDB"));
-            analyzer = new StandardAnalyzer(Version.LUCENE_30);
-            maxFieldLength = IndexWriter.MaxFieldLength.LIMITED;
+            version = Version.LUCENE_30;
+            maxFieldLength = IndexWriter.MaxFieldLength.LIMITED
+            directory = FSDirectory.open(new File("D:\\WebProjects\\qy\\IdeaProjects\\MyLuence\\LuceneDBDB"));
+            analyzer = new StandardAnalyzer(version);
         }catch (Exception e){
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
-
-
     }
-    public LuceneUtil() {};
-
     public static org.apache.lucene.document.Document javaBean2Document(Object object) throws Exception{
-        Class clazz = object.getClass();
-        java.lang.reflect.Field[] fields = clazz.getDeclaredFields();
         org.apache.lucene.document.Document document = new org.apache.lucene.document.Document();
-        for (Field field: fields){
+        Class clazz =  object.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        for(Field field:fields){
             field.setAccessible(true);
             String name = field.getName();
-            String init = name.substring(0,1).toUpperCase();
-            String methodName = "get" + init + name.substring(1);
+            String methodName = "get"+ name.substring(0,1) + name.substring(1);
             Method method = clazz.getDeclaredMethod(methodName,null);
             String value = method.invoke(object,null).toString();
             document.add(new org.apache.lucene.document.Field(name,value, org.apache.lucene.document.Field.Store.YES, org.apache.lucene.document.Field.Index.ANALYZED));
         }
-
-
         return document;
     }
     public static Object document2JavaBean(org.apache.lucene.document.Document document,Class clazz) throws Exception{
+        Object obj = clazz.newInstance();
         Field[] fields = clazz.getDeclaredFields();
-        Object object = new Object();
-        for(Field field : fields){
+        for(Field field:fields){
             field.setAccessible(true);
             String name = field.getName();
-            String value = document.get(name);
-            BeanUtils.setProperty(object,name,value);
+            String value = document.get("name");
+            BeanUtils.setProperty(obj,name,value);
         }
-        return object;
+        return null;
     }
+
 }
